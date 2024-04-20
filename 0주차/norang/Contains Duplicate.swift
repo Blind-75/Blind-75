675532b1e4d0a52c1b14b508e80b9eea280b126e
norang <norang1221@gmail.com>
2024-04-20 16:36:49 +0900

    LeetCode 217 norang 풀이입니다.

---
+++ 0주차/norang/Contains Duplicate.swift
@@ -0,0 +1,21 @@
+Temp
+
+// https://leetcode.com/problems/contains-duplicate/description/
+ 
+// Given an integer array nums, return true if any value appears at least twice in the array, 
+// and return false if every element is distinct.
+
+class Solution {
+    func containsDuplicate(_ nums: [Int]) -> Bool {
+        for index1 in 0..<nums.count-1 {
+            for index2 in index1+1..<nums.count {
+                if index1 != index2 {
+                    if nums[index1] == nums[index2] {
+                        return true
+                    }
+                }
+            }
+        }
+        return false
+    }
+}
