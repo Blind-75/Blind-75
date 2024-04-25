e238f7825f075500a6685b3f0c75fad5548b8d42
Lyosha's MacBook <lyoshasmacbook@10-141-41-67.0101.pos.kr.iosda.org>
2024-04-25 17:19:43 +0900

    LeetCode 271. 'Encode and Decode Strings' Lyosha 풀이입니다.

--- 2주차/Lyosha/LeetCode 271. Encode and Decode Strings.swift
+++ 2주차/Lyosha/LeetCode 271. Encode and Decode Strings.swift
@@ -1,10 +1,11 @@
-// https://leetcode.com/problems/encode-and-decode-strings/description/
 import Foundation
 
 class Codec {
     var charDict: [Character: String] = [:]
     var decodeDict: [String: Character] = [:]
     func encode(_ strs: [String]) -> String {
+        
+        //charDict, decodeDict를 만들기
         var count = 1
         for str in strs{
             for char in str{
@@ -16,31 +17,39 @@ class Codec {
             }
         }
         
+        //만들어진 decodeDict를 바탕으로 encode하기
         var encodedString = ""
         for str in strs{
             for char in str{
                 var encodedChar = self.charDict[char, default: ""]
                 encodedString += " " + encodedChar
-                encodedString += " " + String(257)
             }
+            encodedString += " " + String(257)
         }
         
         return encodedString
     }
     
+    
      func decode(_ s: String) -> [String] {
          let chars = s.components(separatedBy: " ")
          var ans: [String] = []
          var decodedString = ""
+         print(chars)
+
          for char in chars {
-             
+             if char == "" {
+                 continue
+             }
+             if char == String(257){
+                 ans.append(decodedString)
+                 decodedString = ""
+                 continue
+             }
+             let decodedChar: Character = self.decodeDict[char]!
+             decodedString.append(decodedChar)
          }
+         return ans
+         
      }
 }
-
-/**
- * Your Codec object will be instantiated and called as such:
- * let obj = Codec()
- * val s = obj.encode(strs)
- * let ans = obj.decode(s)
- */
