4afcef585e157dd6145887d328b52d591ba2a8e0
Fine <92whdtjs@gmail.com>
2024-07-11 00:53:37 +0900

    Same_Tree 풀이입당

---
+++ 8주차/fine/Same_Tree.swift
@@ -0,0 +1,26 @@
+ public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+    self.val = val
+    self.left = left
+    self.right = right
+     }
+  }
+
+func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+    if p == nil && q == nil {
+        return true
+    }
+    if p == nil || q == nil {
+        return false 
+    }
+    if p?.val != q!.val {
+        return false 
+    }
+
+    return isSameTree(p!.left, q!.left) && isSameTree(p!.right, q!.right)
+}
