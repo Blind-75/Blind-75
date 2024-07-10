e484e44cdbbcf4acee7ad5a21f6e8ebe6977bfc7
Fine <92whdtjs@gmail.com>
2024-07-11 00:25:58 +0900

    Invert_Binary_Tree 풀이임당

--- 8주차/fine/Invert_Binary_Tree.swift
+++ 8주차/fine/Invert_Binary_Tree.swift
@@ -15,6 +15,16 @@
 
 class Solution {
     func invertTree(_ root: TreeNode?) -> TreeNode? {
-        
+        guard let root = root else {
+            return nil 
+        }
+
+        let left = invertTree(root.left)
+        let right = invertTree(root.right)
+
+        root.left = right 
+        root.right = left
+
+        return root 
     }
 }
