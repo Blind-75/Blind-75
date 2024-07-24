026f315ae705bee815e11e19ff43194152477119
Fine <92whdtjs@gmail.com>
2024-07-24 23:16:49 +0900

    Binary_Tree_Level_Order_Traversal.swift

---
+++ 9주차/fine/Binary_Tree_Level_Order_Traversal.swift
@@ -0,0 +1,44 @@
+
+
+ public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+class Solution {
+    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+        guard let root = root else {return []}
+
+        var result: [[Int]] = []
+        var queue: [TreeNode] = [root]
+
+        while !queue.isEmpty {
+            let levelSize = queue.count
+            var currentLevel: [Int] = []
+
+            for _ in 0..<levelSize {
+                let node = queue.removeFirst()
+                currentLevel.append(node.val)
+
+                if let left = node.left {
+                    queue.append(left)
+                }
+
+                if let right = node.right {
+                    queue.append(right)
+                }
+            }
+            result.append(currentLevel)
+
+        }
+        return result 
+    }
+}
