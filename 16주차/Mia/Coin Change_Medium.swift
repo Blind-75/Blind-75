3c7dfdfafc97289cdd43d3567bf504a85c8383eb
Hyun Lee (Mia) <leeseunghyun7799@gmail.com>
2024-10-23 10:33:27 +0900

    LeetCode 91. 'Decode Ways' Mia 풀이입니다.

---
+++ 16주차/Mia/Coin Change_Medium.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func coinChange(_ coins: [Int], _ amount: Int) -> Int {
+        if amount == 0 {
+            return 0
+        }
+
+        var minCoinsForAmt: Array<Int> = Array(repeating: amount + 1, count: amount + 1)
+        minCoinsForAmt[0] = 0 
+
+        for coin in coins{
+            for i in coin...amount{
+                minCoinsForAmt[i] = min(minCoinsForAmt[i], minCoinsForAmt[i - coin] + 1)
+            }
+        }
+        return minCoinsForAmt[amount] == amount + 1 ? -1 : minCoinsForAmt[amount]
+    }
+}
